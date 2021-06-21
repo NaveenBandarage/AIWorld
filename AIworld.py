5b416870f53ac757f7435ce9f0f10007c4940271
Naveen Bandarage <thebanterage@gmail.com>
2021-06-21 23:13:00 +1200

    Tidied up for Xero Grad

--- AIworld.py
+++ AIworld.py
@@ -15,15 +15,15 @@ with io.open(path, encoding='utf-8') as f:
     text = f.read().lower()
 print('Length of text:', len(text))
 
+#sorts all the characters in a unique list so by the time its run it basically like has all the alphabet
 chars = sorted(list(set(text)))
 print('Total number of chars:', len(chars))
 char_indices = dict((c, i) for i, c in enumerate(chars))
 indices_char = dict((i, c) for i, c in enumerate(chars))
 
 
-#source ./venv/bin/activate - THIS IS THE COMMAND TO ACTIVATE VIRTUAL ENVIRONMETN
-# divides the text into sequences of 40 characters. 
-
+# source ./venv/bin/activate - THIS IS THE COMMAND TO ACTIVATE VIRTUAL ENVIRONMETN
+# divides the text into sequences of 40 characters or 40 character chunks. 
 maxlen = 40
 step = 3
 sentencesArray = []
@@ -33,33 +33,32 @@ for i in range(0, len(text) - maxlen, step):
     nextChars.append(text[i + maxlen])
 print('Sequences:', len(sentencesArray))
 
+#turning these 40 character chunks into vectors/matrices for input and output
 print('Now turning into vectors')
-#Getting the x 
+# Getting the x
 x = np.zeros((len(sentencesArray), maxlen, len(chars)), dtype=np.bool)
-#Getting the y
+# Getting the y
 y = np.zeros((len(sentencesArray), len(chars)), dtype=np.bool)
 
-#Iterating through the sentences array 
+# Now that we have sentence arrays we are converting these into numbers (one hot vectorisation)
 for i, sentence in enumerate(sentencesArray):
     for t, char in enumerate(sentence):
         x[i, t, char_indices[char]] = 1
     y[i, char_indices[nextChars[i]]] = 1
 
 
-# Creating a model from single lstm. 
+# Creating a model from single lstm.
 print('Build model...')
-#Taken from tensorflow and github
+# Taken from tensorflow and github
 model = Sequential()
 model.add(LSTM(128, input_shape=(maxlen, len(chars))))
 model.add(Dense(len(chars), activation='softmax'))
-
 optimizer = RMSprop(learning_rate=0.01)
 model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 
 
 def sample(preds, temperature=1.0):
-    
-    # This helps with sampling. 
+    # This helps with sampling.
     preds = np.asarray(preds).astype('float64')
     preds = np.log(preds) / temperature
     exp_preds = np.exp(preds)
@@ -72,7 +71,7 @@ def on_epoch_end(epoch, _):
     # Function invoked at end of each epoch. Prints generated text.
     print()
     print('----- Generating text after Epoch: %d' % epoch)
-
+    #Random starting index with the diversity set 
     start_index = random.randint(0, len(text) - maxlen - 1)
     for diversity in [0.2, 0.5]:
         print('----- diversity:', diversity)
@@ -98,15 +97,16 @@ def on_epoch_end(epoch, _):
             sys.stdout.flush()
         print()
 
+
+#Generating the output based on what we have written
 def generate_output():
     generated = ''
-    
 
     usr_input = input("Write your Travis Scott Lyrics Here: ")
     sentence = ('{0:0>' + str(Tx) + '}').format(usr_input).lower()
-    generated += usr_input 
+    generated += usr_input
 
-    sys.stdout.write("\n\nHere is your verse: \n\n") 
+    sys.stdout.write("\n\nHere is your verse: \n\n")
     sys.stdout.write(usr_input)
     for i in range(400):
 
@@ -117,7 +117,7 @@ def generate_output():
                 x_pred[0, t, char_indices[char]] = 1.
 
         preds = model.predict(x_pred, verbose=0)[0]
-        next_index = sample(preds, temperature = 0.2)
+        next_index = sample(preds, temperature=0.2)
         next_char = indices_char[next_index]
 
         generated += next_char
@@ -129,11 +129,12 @@ def generate_output():
         if next_char == '\n':
             continue
 
+
 print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
 
 model.fit(x, y,
           batch_size=128,
-          epochs=5,
+          epochs=30,
           callbacks=[print_callback])
 
 Tx = 40
--- textCleaner.py
+++ textCleaner.py
@@ -1,15 +1,48 @@
-file1 = open("executeInput.txt","r")
-file2 = open("executeOutput.txt", "w")
+file1 = open("newfile.txt", "r")
+file2 = open("cleanfile.txt", "w")
 
 
-for newLine in file1:
-    # Remove the new line characters and extra spacing
-    newLine = newLine.strip()
-    if not newLine.strip(): continue
-    if not newLine.startswith("["):
-        file2.write(newLine + '\n')
+# for newLine in file1:
+#     # Remove the new line characters and extra spacing
+#     newLine = newLine.strip()
+#     if not newLine.strip(): continue
+#     if not newLine.startswith("["):
+#         continue
+#     newLine.replace("fuck", "")
+#     newLine.replace("bitch", "")
+#     newLine.replace("nigga", "")
+#     newLine.replace("Yeah Yeah Yeah", "")
+#     newLine.replace("shit", "")
+#     newLine.replace("retarded", "")
+#     newLine.replace("pussy", "")
+#     newLine.replace("niggas", "")
 
-    # For each part in the new line look for grade
-    # Move to the next line regardless 
-    
-newLine = file1.readline()
+# f1 = open('newfile.txt', 'r')
+# f2 = open('cleanfile.txt', 'w')
+# for newLine in file1:
+#     newLine.replace("fuck", "")
+#     newLine.replace("bitch", "")
+#     newLine.replace("nigga", "")
+#     newLine.replace("Yeah Yeah Yeah", "")
+#     newLine.replace("shit", "")
+#     newLine.replace("retarded", "")
+#     newLine.replace("pussy", "")
+#     newLine.replace("niggas", "")
+#     file2.write(newLine)
+
+checkWords = ("fuck", "Fuck", "bitch", "Bitch", "Nigga", "nigga",
+              "shit", "retarded", "pussy", "Pussy", "niggas", "Niggas", "   ", "sex", "dick" , "Dick")
+repWords = " "
+
+for line in file1:
+    for check in checkWords:
+        line = line.replace(check, repWords)
+    file2.write(line)
+
+file1.close()
+file2.close()
+
+# For each part in the new line look for grade
+# Move to the next line regardless
+
+# newLine = file1.readline()
